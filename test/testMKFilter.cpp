01a0091c3384e3ec86cc9a3f78538b0a8e18699c
Leph <leph.quentinrouxel@gmail.com>
2020-10-24 11:05:26 +0100

    More tests

--- test/testMKFilter.cpp
+++ test/testMKFilter.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <mkfilter/MKFilter.h>
 
-int main()
+void testLowPass()
 {
     double gain;
     std::vector<double> coefIn;
     std::vector<double> coefOut;
 
     mkfilter::MKFilter(
-        mkfilter::FilterType_t::Bessel,
+        mkfilter::FilterType_t::Butterworth,
         mkfilter::PassType_t::Lowpass,
         0.0,
-        3, 0.25, 0.0,
+        3, 10.0/500.0, 0.0,
         coefIn, coefOut, gain);
 
+    std::cout << "==== LowPass:" << std::endl;
     std::cout << "gain = " << gain << std::endl;
     for (size_t i=0;i<coefIn.size();i++) {
         std::cout << "CoefIn[" << i << "] = " << coefIn[i] << std::endl;
@@ -21,6 +22,59 @@ int main()
     for (size_t i=0;i<coefOut.size();i++) {
         std::cout << "CoefOut[" << i << "] = " << coefOut[i] << std::endl;
     }
+}
+
+void testHighPass()
+{
+    double gain;
+    std::vector<double> coefIn;
+    std::vector<double> coefOut;
+
+    mkfilter::MKFilter(
+        mkfilter::FilterType_t::Butterworth,
+        mkfilter::PassType_t::Highpass,
+        0.0,
+        3, 10.0/500.0, 0.0,
+        coefIn, coefOut, gain);
+
+    std::cout << "==== HighPass:" << std::endl;
+    std::cout << "gain = " << gain << std::endl;
+    for (size_t i=0;i<coefIn.size();i++) {
+        std::cout << "CoefIn[" << i << "] = " << coefIn[i] << std::endl;
+    }
+    for (size_t i=0;i<coefOut.size();i++) {
+        std::cout << "CoefOut[" << i << "] = " << coefOut[i] << std::endl;
+    }
+}
+
+void testBandPass()
+{
+    double gain;
+    std::vector<double> coefIn;
+    std::vector<double> coefOut;
+
+    mkfilter::MKFilter(
+        mkfilter::FilterType_t::Butterworth,
+        mkfilter::PassType_t::Bandpass,
+        0.0,
+        3, 10.0/500.0, 20.0/500.0,
+        coefIn, coefOut, gain);
+
+    std::cout << "==== BandPass:" << std::endl;
+    std::cout << "gain = " << gain << std::endl;
+    for (size_t i=0;i<coefIn.size();i++) {
+        std::cout << "CoefIn[" << i << "] = " << coefIn[i] << std::endl;
+    }
+    for (size_t i=0;i<coefOut.size();i++) {
+        std::cout << "CoefOut[" << i << "] = " << coefOut[i] << std::endl;
+    }
+}
+
+int main()
+{
+    testLowPass();
+    testHighPass();
+    testBandPass();
 
     return 0;
 }
